3614f4f4388b52a92eab3719b2327db5ae706034
trig0 <allengordon0@gmail.com>
2016-11-11 15:17:19 -0600

    fixed audio script

--- chuck.js
+++ chuck.js
@@ -1,68 +1,68 @@
-$(document).ready(function(){
-var URL = 'https://api.icndb.com/jokes/random'
-//state object
+$(document).ready(function() {
+    var URL = 'https://api.icndb.com/jokes/random'
+        //state object
 
-//state modification
+    //state modification
 
 
-//rendering functions
-var audio = new Audio('bang.mp3');
-var chuckSong = new Audio('');
+    //rendering functions
+    var audio = new Audio('bang.mp3');
+    var chuckSong = new Audio('');
 
-$('#js-name-form').hide();
+    $('#js-name-form').hide();
 
-//event listeners
-//1)image 'button' with AJAX 'get'
-$('#js-fist').on('click', function(){
-	audio.play();
-	$.ajax({
-		url: URL,
-		data: {
-			format: 'json'
-		},
-		error: function() {
-			$('#js-quotes').html('<p>Our Kung Fu has encountered an error</p>');
-		},
-		success: function(doj) {
-			$("#js-quotes").html('<p>'+doj.value.joke+'</p>');
-			
-		},
-		type: 'GET'
-		});
-	});
-$('.js-master').on('click', function(){
-	$('#js-name-form').show();
-});
-$('#playButton').on('click' function(){
-	chuckSong.play();
-});
+    //event listeners
+    //1)image 'button' with AJAX 'get'
+    $('#js-fist').on('click', function() {
+        audio.play();
+        $.ajax({
+            url: URL,
+            data: {
+                format: 'json'
+            },
+            error: function() {
+                $('#js-quotes').html('<p>Our Kung Fu has encountered an error</p>');
+            },
+            success: function(doj) {
+                $("#js-quotes").html('<p>' + doj.value.joke + '</p>');
 
-//2)name input
-$('#js-name-form').on('submit', function(event){
-	event.preventDefault();
-	var first = $('.js-first-name').val();
-	var last = $('.js-last-name').val();
-	var nameURL = URL + '?firstName=' + first + '&lastName=' + last;
-	// console.log(nameURL);
-		audio.play();
-	$.ajax({
-		url: nameURL,
-		data: {
-			format: 'json'
-		},
-		error: function() {
-			$('#js-quotes').html('<p>Our Kung Fu has encountered an error</p>');
-		},
-		success: function(data) {
-			$("#js-quotes").html('<p>'+data.value.joke+'</p>');
-			
-		},
-		type: 'GET'
-		});
-	// $('#js-name-form').hide();
-});
+            },
+            type: 'GET'
+        });
+    });
+    $('.js-master').on('click', function() {
+        $('#js-name-form').show();
+    });
+    $('#playButton').on('click', function() {
+        chuckSong.play();
+    });
 
-//3)new quote input
+    //2)name input
+    $('#js-name-form').on('submit', function(event) {
+        event.preventDefault();
+        var first = $('.js-first-name').val();
+        var last = $('.js-last-name').val();
+        var nameURL = URL + '?firstName=' + first + '&lastName=' + last;
+        // console.log(nameURL);
+        audio.play();
+        $.ajax({
+            url: nameURL,
+            data: {
+                format: 'json'
+            },
+            error: function() {
+                $('#js-quotes').html('<p>Our Kung Fu has encountered an error</p>');
+            },
+            success: function(data) {
+                $("#js-quotes").html('<p>' + data.value.joke + '</p>');
+
+            },
+            type: 'GET'
+        });
+        // $('#js-name-form').hide();
+    });
+
+    //3)new quote input
 
 
 });
